48fb1a8b32e78b8768efafeca0ef1f2ac62febd4
Tony Zhang <Johnson@dhcp-10-105-30-133.wireless.northwestern.private>
2019-07-19 12:04:59 -0500

    logic and initial styling

---
+++ assets/js/todo.js
@@ -0,0 +1,33 @@
+
+//check off todos with a crossing or turn it back on
+//has to listen on ul because new li will be created
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//deleting the task
+//listen on ul because of same reason above
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(400, function(){
+		//only executed after the fadeout is completed;
+		$(this).remove(); //this $this is the li, not the span
+	});
+	event.stopPropagation();//this will prevent event bubbling
+});
+
+//logging new todos
+$("input").on("keypress", function(event){
+
+	//check if it is a enter key
+	if(event.which === 13){
+		//enter pressed
+		var newTask = $(this).val();
+		if(newTask.length > 0){
+			var newHTML = "<li><span>X </span>" + newTask + "</li>";
+			$("ul").append(newHTML);
+			$(this).val(""); //clear the input field
+		}
+	}
+
+});
+
